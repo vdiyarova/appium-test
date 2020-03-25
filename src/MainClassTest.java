d677e30990e01db6024669f41e9baf42489969e9
veronika <diyarova.veronika123@gmail.com>
2020-03-25 15:46:36 +0400

    второе задание

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -6,6 +6,14 @@ public class MainClassTest extends MainClass{
     public void testGetLocalNumber(){
         int localNumber = this.getLocalNumber();
         int expectedNumber = 14;
+
         Assert.assertEquals("Local number not equal to" + expectedNumber, localNumber, expectedNumber);
+    }
+
+    @Test
+    public void testGetClassNumber(){
+        int classNumber = this.getClassNumber();
+
+        Assert.assertTrue("Number less than 45", classNumber > 45);
     }
 }
