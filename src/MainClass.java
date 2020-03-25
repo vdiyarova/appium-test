ce5f32939ccbf8634e9dc60a1bcc976db9bf4f90
veronika <diyarova.veronika123@gmail.com>
2020-03-25 16:23:31 +0400

    третье задание

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,6 +1,6 @@
 public class MainClass {
     private int class_number = 20;
-    private String class_string = "Helo, world";
+    private String class_string = "Hello, world";
 
     public int getLocalNumber(){
         return 14;
